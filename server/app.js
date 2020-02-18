6ddbd711abfe42ce773202f920c1ba4683dd3d79
Asten Fuller <astenhfuller@gmail.com>
2020-02-18 13:25:19 -0800

    Tests all passing

--- server/app.js
+++ server/app.js
@@ -43,16 +43,65 @@ app.get('/api/TodoItems', (req, res) => {
 });
 
 app.get('/api/TodoItems/:number', (req, res) => {
-    { mockData[req.params.number] ? 
-        res.status(200).send(mockData[req.params.number]) 
-        : 
-        res.status(404).send('no item found')
-    };
+    let found = false;
+
+    for(let i = 0; i < mockData.length; i++) {
+        if(mockData[i].todoItemId == req.params.number) {
+            res.status(200).send(mockData[i]);
+            found = true;
+        }
+    }
+
+    if(!found) {
+        res.status(404).send('item not fount');
+    }
+
 });
 
 app.post('/api/TodoItems/', (req, res) => {
-   
-})
+    const todoItemId = req.body.todoItemId;
+    const name = req.body.name;
+    const priority = req.body.priority;
+    const completed = req.body.completed;
+    let found = false;
+
+    for(let i = 0; i < mockData.length; i++) {
+        if(mockData[i].todoItemId == todoItemId) {
+            mockData[i].todoItemId = todoItemId;
+            mockData[i].name = name;
+            mockData[i].priority = priority;
+            mockData[i].completed = completed;
+            found = true;
+            res.status(201).send(mockData[i]);
+        }
+    }
+
+    if(!found) {
+        mockData.push({
+            todoItemId: todoItemId,
+            name: name,
+            priority: priority,
+            completed: completed
+        });
+        res.status(201).send(mockData[mockData.length - 1]);
+    }
+});
+
+app.delete('/api/TodoItems/:number', (req, res) => {
+    let found = false;
+
+    for(let i = 0; i< mockData.length; i++) {
+        if(mockData[i].todoItemId == req.params.number) {
+            res.status(200).send(mockData[i]);
+            mockData.splice(i, i + 1);
+            found = true;
+        }
+    }
+
+    if(!found) {
+        res.status(404).send('item not found');
+    }
+});
 
 
 
